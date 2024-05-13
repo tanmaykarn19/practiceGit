462279cfecd5c3dc26997b80fd5764ee859fc431
Tanmay Karn <tanmaykarn58@gmail.com>
2024-05-13 16:57:34 +0530

    added variable e in sub def

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 def greet(name, age):
     return "hello {}. your age is {}".format(name, age)
 
-def subtraction(a, b, c):
-    return a-b
+def subtraction(a, b, c, e):
+    return a-b-c-e
 
 def division(a, b):
     return a/b
